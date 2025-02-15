30ba4fcd073ea06039c1a28bf3324c544b8ed077
=Dat <phamledatctbp@gmail.com>
2025-02-15 16:32:50 +0700

    conffig cors policy

--- source/backend/api_gateway/src/main/java/com/example/api_gateway/configuration/WebClientConfiguration.java
+++ source/backend/api_gateway/src/main/java/com/example/api_gateway/configuration/WebClientConfiguration.java
@@ -2,9 +2,15 @@ package com.example.api_gateway.configuration;
 import com.example.api_gateway.repository.AuthenticationClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.List;
+
 @Configuration
 public class WebClientConfiguration {
     @Bean
@@ -13,7 +19,19 @@ public class WebClientConfiguration {
                 .baseUrl("http://localhost:8080/identity")
                 .build();
     }
+    @Bean
+    CorsWebFilter corsWebFilter(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedMethods(List.of("*"));
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedMethods(List.of("*"));
 
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource
+                = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(urlBasedCorsConfigurationSource);
+    }
     @Bean
     AuthenticationClient authenticationClient(WebClient webClient){
         HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory
