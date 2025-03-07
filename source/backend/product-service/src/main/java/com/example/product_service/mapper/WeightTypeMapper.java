babe06bb52abae8a685b3bc4dc5daaa07bd4b46d
=Dat <phamledatctbp@gmail.com>
2025-03-07 20:55:13 +0700

    create entity weightype

---
+++ source/backend/product-service/src/main/java/com/example/product_service/mapper/WeightTypeMapper.java
@@ -0,0 +1,13 @@
+package com.example.product_service.mapper;
+
+import com.example.product_service.dto.request.WeightTypeRequest;
+import com.example.product_service.dto.request.WeightTypeResponse;
+import com.example.product_service.entity.WeightType;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface WeightTypeMapper {
+    WeightType toWeightType(WeightTypeRequest weightType);
+    WeightTypeResponse toWeightTypeResponse(WeightType weightType);
+    WeightType updateWeightType (long id, WeightTypeRequest request);
+}
